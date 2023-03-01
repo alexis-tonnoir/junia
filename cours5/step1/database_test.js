f85fdfc9763b5c42f5d332e11172c2d7ac3cba8e
Tonnoir <Alexis.tonnoir@miam.tech>
2023-03-01 09:33:00 +0100

    database test

---
+++ cours5/step1/database_test.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'junia_user',
+  password: 'junia_user',
+  database: 'junia',
+  insecureAuth : true
+});
+
+con.connect(err => {
+  if(err) throw err;
+  else console.log('connection established')
+});
+
+con.query('SELECT * FROM annonces', (err, result) => {
+  if(err) throw err;
+  console.log(result);
+});
